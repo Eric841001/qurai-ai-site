7e8a155b92e8f3da0d6b3e4e11327925dd6716b0
Eric841001 <yskang1984@gmail.com>
2026-04-25 20:58:21 +0900

    Update layout.jsx

--- qurai-ai-site/app/layout.jsx
+++ qurai-ai-site/app/layout.jsx
@@ -1,11 +1,16 @@
+import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "QURAI.AI",
   description: "QURAI.AI official website",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
